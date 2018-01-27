af00e25e57643da09f4abf216ae94c0c0a611190
Sausagemancer <ksanner@gmail.com>
2018-01-26 21:43:22 -0800

    Kyle's tools and clickable Interface mods

---
+++ Assets/Scripts/Clickable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Clickable : MonoBehaviour {
+
+    // Use this for initialization
+    BodyController bod;
+    ToolBox toolBox = ToolBox.GetInstance();
+
+    //ToolBoxController tools;
+
+
+	void Start () {
+	    //TODO: Initialize Body Controller	
+        //TODO: Initialize ToolBox Controller
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	} 
+    // On hit, we talk to the owning parent
+    void OnMouseDown() {
+
+        Debug.Log("The parent was clicked! Tag: " + tag + "Name: " + name );
+        switch (tag)
+        {
+            //Body parts:
+            case "Body":
+                //Tell the body that the current Tool has touched it
+
+                break;
+            case "Tool":
+                toolBox.selectTool(name);
+                break;
+            case "Background":
+                toolBox.selectTool("None");
+                break;
+        }
+		// Communicate to owning script:
+	}
+}
---
+++ Assets/Scripts/ToolBox.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ToolBox : MonoBehaviour {
+    public enum Tool
+    {
+        Injector,
+        Tourniquet,
+        Pill,
+        Ointment,
+        None
+    }
+
+    enum Drug
+    {
+        Red,
+        Green,
+        Blue
+    }
+    static ToolBox singleton = null;
+
+    public static ToolBox GetInstance()
+    {
+        if (singleton == null)
+        {
+            singleton = new ToolBox();
+        }
+        return singleton;
+    }
+
+
+     public Tool selectedTool = Tool.None;
+
+    public void selectTool (string toolName)
+    {
+        switch(toolName)
+        {
+            case "Injector":
+                selectedTool = Tool.Injector;
+                break;
+            case "Tourniquet":
+                selectedTool = Tool.Tourniquet;
+                break;
+            case "Pill":
+                selectedTool = Tool.Pill;
+                break;
+            case "Ointment":
+                selectedTool = Tool.Ointment;
+                break;
+            default:
+                selectedTool = Tool.None;
+                break;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
