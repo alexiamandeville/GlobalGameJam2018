fbf20e52b3cb29423417d66177e6cd08a61aaf74
Sausagemancer <ksanner@gmail.com>
2018-01-27 15:42:53 -0800

    Adding BGM to title and loss

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -98,6 +98,7 @@ public class GameController : MonoBehaviour {
         GameObject gameController = GameObject.FindGameObjectWithTag("GameController");
         //TODO: Initialize Body Controller	
         body = gameController.GetComponent(typeof(BodyController)) as BodyController;
+        sound = gameController.GetComponent(typeof(SoundController)) as SoundController;
     }
     // Update is called once per frame
     void Update()
@@ -146,6 +147,7 @@ public class GameController : MonoBehaviour {
 
 	public void ResetGame()
 	{
+        sound.stopBGM();
         //Deactivate uneeded gameobjects and activate needed gameobjects
         isGameFinished = false;
         TitleScreen.SetActive(false);
@@ -167,12 +169,14 @@ public class GameController : MonoBehaviour {
     
     public void ReturnToTitleMenu()
     {
+        sound.playBGM(SoundController.Music.StartScreen);
         GameFinishedScreen.SetActive(false);
         TitleScreen.SetActive(true);
     }
 
     public IEnumerator GoToGameFinishedMenu()
     {
+        sound.playBGM(SoundController.Music.LosingTheme);
         isGameFinished = true;
         HeartScript.StopHeart();
         yield return new WaitForSeconds(2f);
